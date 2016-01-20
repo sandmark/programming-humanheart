1beac915417355f0d8ec640508be26be8cee2c70
sandmark <sandmark.m@gmail.com>
2016-01-20 09:02:42 +0900

    Add TemplateResponder
    Add parts arguments to all response method.

--- lib/responder.rb
+++ lib/responder.rb
@@ -9,25 +9,25 @@ class Responder
   end
 
   protected
-  def response(input, mood)
+  def response(input, parts, mood)
     ''
   end
 end
 
 class WhatResponder < Responder
-  def response(input, mood)
+  def response(input, parts, mood)
     "#{input}ってなに？"
   end
 end
 
 class RandomResponder < Responder
-  def response(input, mood)
+  def response(input, parts, mood)
     select_random(@dictionary.random)
   end
 end
 
 class PatternResponder < Responder
-  def response(input, mood)
+  def response(input, parts, mood)
     @dictionary.pattern.each do |item|
       if m = item.match(input)
         resp = item.choice(mood)
@@ -37,5 +37,21 @@ class PatternResponder < Responder
     end
 
     select_random(@dictionary.random)
+  end
+end
+
+class TemplateResponder < Responder
+  def response(input, parts, mood)
+    keywords = []
+    parts.each do |word, part|
+      keywords.push(word) if Morph::keyword?(part)
+    end
+    count = keywords.size
+    if count > 0 and templates = @dictionary.template(count)
+      template = select_random(templates)
+      template.gsub(/%noun%/){keywords.shift}
+    else
+      select_random(@dictionary.random)
+    end
   end
 end

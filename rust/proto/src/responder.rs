05ec110bef7de6cc903d067fd59e8c4b260203ec
sandmark <sandmark.m@gmail.com>
2017-02-06 13:50:47 +0900

    Fix ファイルから文章を読み込むように変更

--- rust/proto/src/responder.rs
+++ rust/proto/src/responder.rs
@@ -1,6 +1,21 @@
 extern crate rand;
 
 use self::rand::Rng;
+use std::fs;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn select_random(v: &Vec<String>) -> String {
+    let len = v.len();
+    if len == 0 {
+        String::new()
+    } else {
+        let n = rand::thread_rng().gen_range(0, len);
+        v[n].clone()
+    }
+}
 
 pub trait Responder {
     fn response(&self, input: String) -> String;
@@ -13,7 +28,7 @@ pub struct WhatResponder {
 
 pub struct RandomResponder {
     name: String,
-    responses: Vec<String>,
+    phrases: Vec<String>,
 }
 
 impl Responder for WhatResponder {
@@ -32,8 +47,7 @@ impl Responder for RandomResponder {
     }
 
     fn response(&self, input: String) -> String {
-        let n = rand::thread_rng().gen_range(0, self.responses.len());
-        self.responses[n].clone()
+        select_random(&self.phrases)
     }
 }
 
@@ -45,12 +59,61 @@ impl WhatResponder {
 
 impl RandomResponder {
     pub fn new(name: &str) -> RandomResponder {
-        let v =
-            vec!["今日はさむいね","チョコたべたい","きのう10円ひろった"];
-        let v = v.iter().map(|&x| x.to_string()).collect::<Vec<String>>();
         RandomResponder {
             name: name.to_string(),
-            responses: v,
+            phrases: RandomResponder::load_dic(),
+        }
+    }
+
+    // 空辞書を作成 or ロード
+    fn load_dic() -> Vec<String> {
+        let dict = &Path::new("dics/random.txt");
+
+        // 空の辞書ファイルを作成。存在する場合は無視
+        RandomResponder::check_dict("dics", dict);
+
+        // ファイルの内容を読み込む
+        let s = match RandomResponder::read_file(dict) {
+            Err(e) => panic!("! {:?}", e),
+            Ok(s)  => s,
+        };
+
+        // '(\r)\n' による分割
+        let lines = s.lines();
+        let mut res: Vec<String> = Vec::new();
+        for line in lines {
+            match line {
+                "" => continue,
+                _  => res.push(line.to_string()),
+            }
+        }
+        res
+    }
+
+    // 辞書・ディレクトリのチェック
+    // 無ければ作成し、あれば何もしない
+    fn check_dict(dir: &str, path: &Path) {
+        if !&Path::new(dir).exists() {
+            // ディレクトリの作成
+            fs::create_dir(dir).unwrap_or_else(|why| {
+                panic!("failed to create directory: {:?}", why);
+            });
+        }
+
+        if !path.exists() {
+            File::create(path).unwrap_or_else(|why| {
+                panic!("faild to create {}: {:?}", path.display(), why);
+            });
+        }
+    }
+
+    // ファイル読み込み
+    fn read_file(path: &Path) -> io::Result<String> {
+        let mut f = try!(File::open(path));
+        let mut s = String::new();
+        match f.read_to_string(&mut s) {
+            Err(e) => Err(e),
+            Ok(_)  => Ok(s),
         }
     }
 }
